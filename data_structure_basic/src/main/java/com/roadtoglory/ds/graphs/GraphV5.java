3dc6def724b659ad079a7f4b75f981caef17e128
Subhendu123 <subhendu.sd.das@gmail.com>
2026-02-18 18:56:45 +0530

    Dijkstra Implementation - Graph v5

---
+++ data_structure_basic/src/main/java/com/roadtoglory/ds/graphs/GraphV5.java
@@ -0,0 +1,158 @@
+/*
+ * Copyright (c) 2026.
+ * This is created and managed by $(git config user.name)
+ */
+
+package com.roadtoglory.ds.graphs;
+/*
+*
+*
+
+This class GraphV5 is created and managed by subhe
+Created on 16-02-2026 at 08:00 for the project Weighted Undirected Graph Implementation - Udemy
+
+*
+*
+*/
+
+import java.util.*;
+
+public class GraphV5 {
+
+    private List<List<Integer>> graphAdjList;
+    private Map<String, Integer> edgeWeights;
+
+    private Integer getWeight(Integer vertex, Integer edge) {
+//        String key = String.valueOf(new StringBuilder(vertex).append("-").append(edge));
+        String key = vertex + "-" + edge;
+        if (edgeWeights.containsKey(key)) {
+            return edgeWeights.get(key);
+        }
+        else {
+            key = edge + "-" + vertex;
+            return edgeWeights.get(key);
+        }
+    }
+
+    private void setWeight(Integer vertex, Integer edge, Integer weight) {
+        String key = vertex + "-" + edge;
+        if (edgeWeights == null) {
+            edgeWeights = new HashMap<>();
+        }
+        edgeWeights.put(key, weight);
+    }
+
+    public void add(Integer vertex) {
+        if (this.graphAdjList == null) {
+            this.graphAdjList = new ArrayList<>();
+        }
+        if (vertex >= this.graphAdjList.size()) {
+            this.graphAdjList.add(new ArrayList<>());
+        }
+
+    }
+
+    public void add(Integer vertex, Integer edge) {
+        add(vertex);
+        add(edge);
+        this.graphAdjList.get(vertex).add(edge);
+        this.graphAdjList.get(edge).add(vertex);
+    }
+
+    public void add(Integer vertex, Integer edge, Integer weight) {
+        add(vertex, edge);
+        setWeight(vertex, edge, weight);
+    }
+
+    public void printGraph() {
+        for (int i = 0; i < this.graphAdjList.size(); i++) {
+            System.out.println("[ " + i + " -> " + this.graphAdjList.get(i) + " ]");
+        }
+        // print the weight
+        for (Map.Entry<String, Integer> entry : edgeWeights.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+    }
+
+    public Integer minSpanningTree() {
+
+        Set<Integer> includedMST = new HashSet<>();
+        Set<Integer> allVertex = new HashSet<>();
+        for (int i = 0; i < this.graphAdjList.size(); i++) {
+            if (i != 0) {
+                allVertex.add(i);
+            }
+        }
+        int result = 0;
+        for (int i = 0; i < this.graphAdjList.size(); i++) {
+
+            includedMST.add(i);
+//            if (includedMST.add(i)) {
+            int lowestWeight = 0;
+            int lowestVertex = -1;
+            int selectedMst = i;
+            // find the lowest weighted edge connected to this tree includedMST
+            for (Integer mstV : includedMST) {
+
+                for (Integer vertex : allVertex) {
+                    List<Integer> connectedV = this.graphAdjList.get(vertex);
+                    if (!mstV.equals(vertex) && connectedV.contains(mstV)) {
+                        int weight = getWeight(mstV, vertex);
+                        if (lowestWeight == 0) {
+                            lowestWeight = weight;
+                            lowestVertex = vertex;
+                            selectedMst = mstV;
+                        }
+                        else if (lowestWeight > weight) {
+                            lowestWeight = weight;
+                            lowestVertex = vertex;
+                            selectedMst = mstV;
+
+                        }
+
+                    }
+                }
+            }
+
+            if (lowestVertex > -1 && lowestWeight > 0) {
+
+                includedMST.add(lowestVertex);
+                result += lowestWeight;
+//                    visitedV[lowestVertex] = true;
+                allVertex.remove(lowestVertex);
+                System.out.println(
+                        "The  vertex [" + lowestVertex + "] and the corresponding weight [" + lowestWeight + "] are added to the existing vertex --> " + selectedMst + " <-- Min Spanning Tree ");
+            }
+
+//            visitedV[i] = true;
+        }
+
+
+//        }
+        System.out.println("The min spanning tree graph is " + includedMST);
+        System.out.println("The min edge weight count " + result);
+        return result;
+    }
+
+    public Integer[] dijkstraShortestDistance() {
+        Integer[] distanceV = new Integer[this.graphAdjList.size()];
+        Arrays.fill(distanceV, Integer.MAX_VALUE);
+        distanceV[0] = 0;
+
+        for (int i = 0; i < this.graphAdjList.size(); i++) {
+            List<Integer> neiV = this.graphAdjList.get(i);
+
+            for (Integer nEdge : neiV) {
+                int edgeWeight = this.getWeight(i, nEdge);
+                int currDistance = distanceV[nEdge];
+                if (currDistance > edgeWeight + distanceV[i]) {
+                    currDistance = edgeWeight + distanceV[i];
+                    distanceV[nEdge] = currDistance;
+                }
+
+            }
+        }
+        return distanceV;
+    }
+
+}
---
+++ data_structure_basic/src/main/java/com/roadtoglory/ds/testing/graphs/GraphV5JunitTesting.java
@@ -0,0 +1,222 @@
+/*
+ * Copyright (c) 2026.
+ * This is created and managed by $(git config user.name)
+ */
+
+package com.roadtoglory.ds.testing.graphs;
+/*
+*
+*
+
+This class GraphV5JunitTesting is created and managed by subhe
+Created on 16-02-2026 at 16:33 for the project undirected weighted graph implementation
+
+*
+*
+*/
+
+import com.roadtoglory.ds.graphs.GraphV5;
+import org.junit.Test;
+import org.junit.jupiter.api.Order;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GraphV5JunitTesting {
+
+
+    @Test
+    @Order(1)
+    public void sanity_check_001() {
+
+        GraphV5 graph = new GraphV5();
+        graph.add(0);
+        graph.add(0, 1, 2);
+        graph.add(2);
+        graph.add(0, 2, 6);
+        graph.add(1, 2, 3);
+        graph.add(1, 3, 8);
+        graph.add(1, 4, 5);
+        graph.add(2, 4, 7);
+
+        graph.printGraph();
+
+    }
+
+    @Test
+    @Order(2)
+    public void min_spanning_tree_tc_01() {
+
+        GraphV5 graph = new GraphV5();
+        graph.add(0);
+        graph.add(0, 1, 2);
+        graph.add(2);
+        graph.add(1, 2, 3);
+        graph.add(3);
+        graph.add(0, 3, 6);
+        graph.add(1, 3, 8);
+        graph.add(1, 4, 5);
+        graph.add(2, 4, 7);
+
+        graph.printGraph();
+        int minTotalWeight = graph.minSpanningTree();
+        assertEquals(minTotalWeight, 16);
+
+    }
+
+    @Test
+    @Order(3)
+    public void shortest_path_Dijkstra_tc_01() {
+
+        GraphV5 graph = new GraphV5();
+        graph.add(0);
+        graph.add(0, 1, 4);
+        graph.add(2);
+        graph.add(0, 2, 1);
+        graph.add(1, 2, 2);
+
+        graph.add(3);
+        graph.add(2, 3, 4);
+        graph.add(1, 3, 2);
+
+        graph.add(3, 4, 1);
+        graph.add(2, 4, 5);
+
+        graph.printGraph();
+        Integer[] shortestDistanceArr = graph.dijkstraShortestDistance();
+        for (int i = 0; i < shortestDistanceArr.length; i++) {
+            System.out.println(i + " -> " + shortestDistanceArr[i]);
+        }
+        assertEquals(shortestDistanceArr[0], 0);
+        assertEquals(shortestDistanceArr[1], 3);
+        assertEquals(shortestDistanceArr[2], 1);
+        assertEquals(shortestDistanceArr[3], 5);
+        assertEquals(shortestDistanceArr[4], 6);
+
+    }
+
+    @Test
+    @Order(4)
+    public void shortest_path_Dijkstra_tc_02() {
+
+        GraphV5 graph = new GraphV5();
+        graph.add(0);
+        graph.add(0, 1, 2);
+        graph.add(2);
+        graph.add(1, 2, 3);
+        graph.add(3);
+        graph.add(0, 3, 6);
+        graph.add(1, 3, 1);
+        graph.add(1, 4, 5);
+        graph.add(2, 4, 1);
+
+        graph.printGraph();
+        Integer[] shortestDistanceArr = graph.dijkstraShortestDistance();
+        for (int i = 0; i < shortestDistanceArr.length; i++) {
+            System.out.println(i + " -> " + shortestDistanceArr[i]);
+        }
+
+        assertEquals(shortestDistanceArr[0], 0);
+        assertEquals(shortestDistanceArr[1], 2);
+        assertEquals(shortestDistanceArr[2], 5);
+        assertEquals(shortestDistanceArr[3], 3);
+        assertEquals(shortestDistanceArr[4], 6);
+
+    }
+
+    @Test
+    @Order(5)
+    public void shortest_path_Dijkstra_tc_03() {
+
+        GraphV5 graph = new GraphV5();
+        graph.add(0);
+        graph.add(0, 1, 1);
+        graph.add(2);
+        graph.add(0, 2, 4);
+        graph.add(3);
+        graph.add(2, 3, 1);
+        graph.add(1, 3, 2);
+
+        graph.printGraph();
+        Integer[] shortestDistanceArr = graph.dijkstraShortestDistance();
+        for (int i = 0; i < shortestDistanceArr.length; i++) {
+            System.out.println(i + " -> " + shortestDistanceArr[i]);
+        }
+
+        assertEquals(shortestDistanceArr[0], 0);
+        assertEquals(shortestDistanceArr[1], 1);
+        assertEquals(shortestDistanceArr[2], 4);
+        assertEquals(shortestDistanceArr[3], 3);
+
+    }
+
+    @Test
+    @Order(6)
+    public void shortest_path_Dijkstra_tc_04() {
+
+        GraphV5 graph = new GraphV5();
+        graph.add(0);
+        graph.add(0, 1, 10);
+        graph.add(2);
+        graph.add(0, 2, 2);
+        graph.add(1, 2, 1);
+
+        graph.printGraph();
+        Integer[] shortestDistanceArr = graph.dijkstraShortestDistance();
+        for (int i = 0; i < shortestDistanceArr.length; i++) {
+            System.out.println(i + " -> " + shortestDistanceArr[i]);
+        }
+
+        assertEquals(shortestDistanceArr[0], 0);
+        assertEquals(shortestDistanceArr[1], 3);
+        assertEquals(shortestDistanceArr[2], 2);
+
+    }
+
+
+    // King of King Test case
+    @Test
+    @Order(7)
+    public void shortest_path_Dijkstra_tc_05() {
+
+        GraphV5 graph = new GraphV5();
+        graph.add(0);
+        graph.add(0, 1, 4);
+        graph.add(2);
+        graph.add(0, 2, 8);
+        graph.add(1, 2, 11);
+        graph.add(3);
+        graph.add(4);
+        graph.add(1, 3, 8);
+        graph.add(2, 4, 7);
+        graph.add(3, 4, 2);
+        graph.add(4, 5, 6);
+        graph.add(2, 5, 1);
+        graph.add(6);
+        graph.add(7);
+        graph.add(8);
+        graph.add(3, 6, 7);
+        graph.add(3, 7, 4);
+        graph.add(5, 7, 2);
+        graph.add(6, 7, 14);
+        graph.add(6, 8, 9);
+        graph.add(7, 8, 10);
+
+
+        graph.printGraph();
+        Integer[] shortestDistanceArr = graph.dijkstraShortestDistance();
+        for (int i = 0; i < shortestDistanceArr.length; i++) {
+            System.out.println(i + " -> " + shortestDistanceArr[i]);
+        }
+
+        assertEquals(shortestDistanceArr[0], 0);
+        assertEquals(shortestDistanceArr[1], 4);
+        assertEquals(shortestDistanceArr[2], 8);
+        assertEquals(shortestDistanceArr[3], 12);
+        assertEquals(shortestDistanceArr[4], 14);
+        assertEquals(shortestDistanceArr[5], 9);
+        assertEquals(shortestDistanceArr[6], 19);
+        assertEquals(shortestDistanceArr[7], 11);
+        assertEquals(shortestDistanceArr[8], 21);
+
+    }
+}
